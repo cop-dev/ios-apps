21877acdecb0c0eb96be239407c255fee1e627d7
huzongyao <huzongyao@tangbull.com>
2022-09-21 11:11:07 +0800

    fix page with vue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
+import json
 import os
 import re
-import shutil
 
 from openpyxl import load_workbook
 
@@ -10,15 +10,12 @@ from openpyxl import load_workbook
 
 
 class IOSAppDeploy:
-    SITE_ROOT = 'https://cop-dev.github.io/ios-apps/'
-    DIST_PATH = './dist/'
+    SITE_ROOT = 'https://cop-dev.github.io/ios-apps/plist/'
+    DIST_PATH = './dist/plist/'
     EXCEL_PATH = './apps.xlsx'
-    TOP_DOC_PATH = './tpl/root.html'
-    SUB_DOC_PATH = './tpl/sub.html'
     PLIST_TPL_PATH = './tpl/plist.xml'
 
     def __init__(self):
-        self.sub_tpl = self._load_doc_tpl(self.SUB_DOC_PATH)
         self.plist_tpl = self._load_doc_tpl(self.PLIST_TPL_PATH)
 
     @staticmethod
@@ -58,24 +55,23 @@ class IOSAppDeploy:
     def generate_single_sheet(self, sheet_title, sheet, out_path):
         if not os.path.exists(out_path):
             os.makedirs(out_path)
-        html_txt = ''
-        sub_doc = self.sub_tpl
+        js_obj = {'name': sheet_title}
+        js_list = []
         for idx, row in enumerate(sheet.rows):
             if idx == 0:
                 continue
             name = row[0].value
+            bundle_id = row[1].value
             version = row[2].value
             plist_name = self.generate_some_plist(idx, row, out_path)
             plist_path = '%s%s/%s' % (self.SITE_ROOT, sheet_title, plist_name)
             href = 'itms-services://?action=download-manifest&url=%s' % plist_path
-            html_txt += '<li><a href="%s">%s(%s)</a></li>\n' % (href, name, version)
-        if not html_txt:
-            html_txt = 'No Items!!'
-        txt = sub_doc.replace('%list_content%', html_txt)
-        txt = txt.replace('%title%', sheet.title)
-        html_path = '%sindex.html' % out_path
-        with open(html_path, 'w', encoding='utf-8') as f:
-            f.write(txt)
+            js_list.append(
+                {'category': sheet_title, 'name': name, 'version': version, 'href': href, 'bundle_id': bundle_id})
+        js_obj['apps'] = js_list
+        js_path = '%s/index.json' % out_path
+        with open(js_path, 'w', encoding='utf-8') as f:
+            json.dump(js_obj, f)
 
     def page_generate_work(self):
         if not os.path.exists(self.EXCEL_PATH):
@@ -83,22 +79,19 @@ class IOSAppDeploy:
             return
         if not os.path.exists(self.DIST_PATH):
             os.makedirs(self.DIST_PATH)
-        if not os.path.exists('%sqrcode.png' % self.DIST_PATH):
-            shutil.copy('./tpl/qrcode.png', self.DIST_PATH)
         wb = load_workbook(self.EXCEL_PATH)
-        html_txt = ''
-        top_doc = self._load_doc_tpl(self.TOP_DOC_PATH)
+        time_txt = datetime.datetime.now().strftime("%Y%m%d %H%M%S")
+        js_obj = {'time': time_txt}
+        js_list = []
         for sheet in wb:
             sheet_title = self.check_file_name(sheet.title)
             sheet_path = '%s%s/' % (self.DIST_PATH, sheet_title)
-            html_txt += '<li><a href="./%s/index.html">%s</a></li>\n' % (sheet_title, sheet_title)
             self.generate_single_sheet(sheet_title, sheet, sheet_path)
-        txt = top_doc.replace('%list_content%', html_txt)
-        time_txt = datetime.datetime.now().strftime("%Y%m%d %H%M%S")
-        txt = txt.replace('%update_date%', time_txt)
-        html_path = '%sindex.html' % self.DIST_PATH
-        with open(html_path, 'w', encoding='utf-8') as f:
-            f.write(txt)
+            js_list.append({'name': sheet_title, 'count': sheet.max_row - 1})
+        js_obj['list'] = js_list
+        js_path = '%sindex.json' % self.DIST_PATH
+        with open(js_path, 'w', encoding='utf-8') as f:
+            json.dump(js_obj, f)
 
 
 if __name__ == "__main__":
