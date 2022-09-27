f7d800d2b0a95bfaf086576acec01c34402469fe
huzongyao <huzongyao@tangbull.com>
2022-09-27 16:40:15 +0800

    to avoid static

--- src/http/index.js
+++ src/http/index.js
@@ -5,4 +5,19 @@ http.defaults.baseURL = process.env.VUE_APP_HOST_URL;
 
 http.defaults.timeout = 16000;
 
+// 添加请求拦截器
+http.interceptors.request.use(config => {
+  let dateTime = Date.now();
+  if (config.method === 'post') {
+  } else if (config.method === 'get') {
+    config.params = {
+      ...config.params,
+      '_t0': String(dateTime),
+    };
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
 export default http;
